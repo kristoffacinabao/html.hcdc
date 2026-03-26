0f3b723c9f1f8805ed299ddfe1ba9c7779da9858
kristoffacinabao <kristoff.facinabao@hcdc.edu.ph>
2026-03-26 10:13:38 +0800

    Add files via upload

---
+++ kk.js
@@ -0,0 +1,10 @@
+const alertBtn = document.querySelector('.alert-btn');
+alertBtn.addEventListener('click', () => {
+  alert('Click me');
+});
+
+
+const exploreBtn = document.querySelector('.primary-btn');
+exploreBtn.addEventListener('click', () => {
+  alert('Exploring HCDC...');
+});
